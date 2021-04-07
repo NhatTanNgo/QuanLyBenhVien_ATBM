ead1507a07ac15541bb963799a2fe554737d1226
Leo-TheCoder <64176716+Leo-TheCoder@users.noreply.github.com>
2021-04-07 17:59:20 +0700

    Initial Commit

---
+++ PhanHe01/PhanHe01/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhanHe01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int tabSelection = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ManageUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(tabSelection == 0)
+            {
+                return;
+            }
+
+            tabSelection = 0;
+            ManagePrivilege.Background = Brushes.Gray;
+            ManageUserButton.Background = Brushes.White;
+        }
+
+        private void ManagePrivilege_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabSelection == 1)
+            {
+                return;
+            }
+
+            tabSelection = 1;
+            ManagePrivilege.Background = Brushes.White;
+            ManageUserButton.Background = Brushes.Gray;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tabSelection = 0;
+            ManagePrivilege.Background = Brushes.Gray;
+            ManageUserButton.Background = Brushes.White;
+        }
+    }
+}
