67744467de2b721fb875590e7f523cecfe1c51a0
Leo-TheCoder <64176716+Leo-TheCoder@users.noreply.github.com>
2021-06-10 20:26:44 +0700

    GUI QLTaiVu & QLTaiNguyen

---
+++ HeThongBenhVien/HeThongBenhVien/FinanceManagement/GUI_FinStaffsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeThongBenhVien.FinanceManagement
+{
+    /// <summary>
+    /// Interaction logic for GUI_FinStaffsPage.xaml
+    /// </summary>
+    public partial class GUI_FinStaffsPage : Page
+    {
+        public GUI_FinStaffsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HeThongBenhVien/HeThongBenhVien/FinanceManagement/GUI_ServicePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeThongBenhVien.FinanceManagement
+{
+    /// <summary>
+    /// Interaction logic for GUI_ServicePage.xaml
+    /// </summary>
+    public partial class GUI_ServicePage : Page
+    {
+        public GUI_ServicePage()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
