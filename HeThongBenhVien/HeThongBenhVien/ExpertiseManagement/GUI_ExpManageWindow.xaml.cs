a3a1fef44ce037e1e806d486c9c8db0967c3d2e0
Leo-TheCoder <64176716+Leo-TheCoder@users.noreply.github.com>
2021-06-11 14:47:54 +0700

    GUI QLChuyenMon & TiepTan

---
+++ HeThongBenhVien/HeThongBenhVien/ExpertiseManagement/GUI_ExpManageWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeThongBenhVien.ExpertiseManagement
+{
+    /// <summary>
+    /// Interaction logic for GUI_ExpManageWindow.xaml
+    /// </summary>
+    public partial class GUI_ExpManageWindow : Window
+    {
+        private int tabSelection = 0;
+        private String username = "NULL";
+        private GUI_ExpertisePage expertisePage = new GUI_ExpertisePage();
+
+        public GUI_ExpManageWindow()
+        {
+            InitializeComponent();
+        }
+
+        public GUI_ExpManageWindow(String _username)
+        {
+            InitializeComponent();
+            username = _username;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tabSelection = 0;
+            ManageExpertiseButton.Background = Brushes.White;
+            OtherButton.Background = Brushes.LightGray;
+            MainFrame.Navigate(expertisePage);
+            LoginUsernameTextBlock.Text = "Hello " + username.ToUpper();
+        }
+
+        private void ManageExpertiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabSelection == 0)
+            {
+                return;
+            }
+
+            tabSelection = 0;
+            ManageExpertiseButton.Background = Brushes.White;
+            OtherButton.Background = Brushes.LightGray;
+            MainFrame.Navigate(expertisePage);
+        }
+
+        private void OtherButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabSelection == 1)
+            {
+                return;
+            }
+
+            tabSelection = 1;
+            ManageExpertiseButton.Background = Brushes.LightGray;
+            OtherButton.Background = Brushes.White;
+        }
+    }
+}
---
+++ HeThongBenhVien/HeThongBenhVien/Reception/GUI_ReceptionWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeThongBenhVien.Reception
+{
+    /// <summary>
+    /// Interaction logic for GUI_ReceptionWindow.xaml
+    /// </summary>
+    public partial class GUI_ReceptionWindow : Window
+    {
+        private int tabSelection = 0;
+        private String username = "NULL";
+        private GUI_AddProfile addProfilePage = new GUI_AddProfile();
+        private GUI_ManageProfile manageProfile = new GUI_ManageProfile();
+
+        public GUI_ReceptionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(tabSelection == 0)
+            {
+                return;
+            }    
+
+            tabSelection = 0;
+            AddProfileButton.Background = Brushes.White;
+            ManageProfileButton.Background = Brushes.LightGray;
+            OtherButton.Background = Brushes.LightGray;
+            MainFrame.Navigate(addProfilePage);
+        }
+
+        private void ManageProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabSelection == 1)
+            {
+                return;
+            }
+
+            tabSelection = 1;
+            AddProfileButton.Background = Brushes.LightGray;
+            ManageProfileButton.Background = Brushes.White;
+            OtherButton.Background = Brushes.LightGray;
+            MainFrame.Navigate(manageProfile);
+        }
+
+        private void OtherButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabSelection == 2)
+            {
+                return;
+            }
+
+            tabSelection = 2;
+            AddProfileButton.Background = Brushes.LightGray;
+            ManageProfileButton.Background = Brushes.LightGray;
+            OtherButton.Background = Brushes.White;
+            //MainFrame.Navigate(addProfilePage);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tabSelection = 0;
+            AddProfileButton.Background = Brushes.White;
+            ManageProfileButton.Background = Brushes.LightGray;
+            OtherButton.Background = Brushes.LightGray;
+            MainFrame.Navigate(addProfilePage);
+            LoginUsernameTextBlock.Text = "Hello " + username.ToUpper();
+        }
+    }
+}
