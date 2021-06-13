a7ae2ac53dd6db4636cfcf942234fd20f6f5e4c4
NhatTai20 <ntai20111@gmail.com>
2021-06-13 22:54:26 +0700

    update Doctor Window

--- HeThongBenhVien/DAO/DBConnect.cs
+++ HeThongBenhVien/DAO/DBConnect.cs
@@ -10,7 +10,7 @@ namespace DAO
     public class DBConnect
     {
         //protected static OracleConnection _conn = null;
-        protected static OracleConnection _conn = new OracleConnection("Data Source=localhost:1521/xepdb1;Persist Security Info=True;User ID = OT; Password= minhlamv18");
+        protected static OracleConnection _conn = new OracleConnection("Data Source=localhost:1521/xepdb1;Persist Security Info=True;User ID = OT; Password= 123");
         protected static String _username = null;
 
         //Data Source = IP:Port/Database name (SID)
@@ -19,7 +19,7 @@ namespace DAO
         public static void InitConnection(String username, String password, String dbName)
         {
             //String connectionString = $"Data Source=localhost:1521/{dbName};Persist Security Info=True;User ID = {username}; Password={password}";
-            String connectionString = $"Data Source=localhost:1521/xepdb1;Persist Security Info=True;User ID = OT; Password= minhlamv18";
+            String connectionString = $"Data Source=localhost:1521/xepdb1;Persist Security Info=True;User ID = OT; Password= 123";
 
             OracleConnection connection = null;
             try
