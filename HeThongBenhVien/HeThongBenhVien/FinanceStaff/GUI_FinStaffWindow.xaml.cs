907e0a042ed141f71272ecf6e7bb4abcb8b0f186
NhatTai20 <ntai20111@gmail.com>
2021-06-15 00:17:33 +0700

    Adding folders Create PharmacySeller Window

---
+++ HeThongBenhVien/HeThongBenhVien/FinanceStaff/GUI_FinStaffWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeThongBenhVien.FinanceStaff
+{
+    /// <summary>
+    /// Interaction logic for GUI_FinStaffWindow.xaml
+    /// </summary>
+    public partial class GUI_FinStaffWindow : Window
+    {
+        public GUI_FinStaffWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HeThongBenhVien/HeThongBenhVien/PharmacySeller/GUI_PharmaSellerWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeThongBenhVien.PharmacySeller
+{
+    /// <summary>
+    /// Interaction logic for GUI_PharmaSellerWindow.xaml
+    /// </summary>
+    public partial class GUI_PharmaSellerWindow : Window
+    {
+        public GUI_PharmaSellerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
